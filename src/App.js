9f749c344465497aa5011e42490eb60e3623f23f
Vinyas Hegde <46837876+vinyashegde@users.noreply.github.com>
2023-06-27 22:17:38 +0000

    Update 8 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import Spline from '@splinetool/react-spline';
+import Spline from "@splinetool/react-spline";
+import { useEffect, useState } from "react";
+import NavbarWithCTAButton from "./components/navbar";
+import HorizontalCard from "./components/aboutme";
+import DefaultTimeline from "./components/timline";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Adjust the breakpoint as per your requirements
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="flex w-screen h-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
-      <div className="relative" id="home">
-      <Spline scene="https://prod.spline.design/zkXRDXVktXSaQLT4/scene.splinecode" />
+    <div>
+      <div className="flex flex-col items-center justify-start min-h-screen bg-primary pb-20">
+        <div className="w-3/4 mt-6">
+          <NavbarWithCTAButton></NavbarWithCTAButton>
+        </div>
+
+        <div
+          className="relative w-full h-full items-center justify-center"
+          style={{
+            marginTop: "calc(100vh * 0.1)",
+            width: isMobile ? "100%" : "100%",
+            height: isMobile ? "auto" : "calc(100vh - (100vh * 0.1))",
+          }}
+        >
+          <Spline scene="https://prod.spline.design/zkXRDXVktXSaQLT4/scene.splinecode" />
+        </div>
+
+        <div className="max-w-screen-lg mx-auto mt-8">
+          <HorizontalCard />
+        </div>
+
+        {/* <div>
+          <DefaultTimeline />
+        </div> */}
       </div>
     </div>
   );
