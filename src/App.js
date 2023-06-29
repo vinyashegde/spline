ac3f99ee1de64cc3a1ced4f97dba75fa985a87ab
Vinyas Hegde <46837876+vinyashegde@users.noreply.github.com>
2023-06-29 08:13:58 +0000

    Update 8 files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Spline from "@splinetool/react-spline";
 import { useEffect, useState } from "react";
 import NavbarWithCTAButton from "./components/navbar";
-import HorizontalCard from "./components/aboutme";
-import DefaultTimeline from "./components/timline";
+import AboutCode from "./components/aboutcode";
+import GithubCard from "./components/githubcard";
+import AnimatedCursor from "react-animated-cursor";
+import FootCode from "./components/foot";
 
-function App() {
-  const [isMobile, setIsMobile] = useState(false);
-
+function ResizeHandler({ setIsMobile }) {
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768); // Adjust the breakpoint as per your requirements
@@ -18,14 +18,26 @@ function App() {
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, []);
+  }, [setIsMobile]);
+
+  return null;
+}
+
+function App() {
+  const [isMobile, setIsMobile] = useState(false);
 
   return (
     <div>
+      <div className="App">
+        <AnimatedCursor />
+      </div>
+
       <div className="flex flex-col items-center justify-start min-h-screen bg-primary pb-20">
-        <div className="w-3/4 mt-6">
+        <div className="w-11/12 mt-4">
           <NavbarWithCTAButton></NavbarWithCTAButton>
         </div>
+
+        <ResizeHandler setIsMobile={setIsMobile} />
 
         <div
           className="relative w-full h-full items-center justify-center"
@@ -35,17 +47,47 @@ function App() {
             height: isMobile ? "auto" : "calc(100vh - (100vh * 0.1))",
           }}
         >
-          <Spline scene="https://prod.spline.design/zkXRDXVktXSaQLT4/scene.splinecode" />
+          <Spline scene="https://draft.spline.design/qsvYhuKccbhtisLS/scene.splinecode" />
         </div>
 
-        <div className="max-w-screen-lg mx-auto mt-8">
-          <HorizontalCard />
+        <div className="min-w-screen min-h-screen flex items-center justify-center m-4">
+          <AboutCode />
         </div>
 
-        {/* <div>
-          <DefaultTimeline />
-        </div> */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 m-7">
+          <div className={isMobile ? "w-full" : ""}>
+            <GithubCard
+              title="Project Title"
+              description="This is a description of the project. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque scelerisque sagittis."
+              htmlText="HTML"
+              cssText="CSS"
+              jsText="JS"
+            />
+          </div>
+          <div className={isMobile ? "w-full" : ""}>
+            <GithubCard />
+          </div>
+          <div className={isMobile ? "w-full" : ""}>
+            <GithubCard />
+          </div>
+          <div>
+            <div className={isMobile ? "w-full" : ""}>
+              <GithubCard />
+            </div>
+          </div>
+          <div>
+            <div className={isMobile ? "w-full" : ""}>
+              <GithubCard />
+            </div>
+          </div>
+          <div>
+            <div className={isMobile ? "w-full" : ""}>
+              <GithubCard />
+            </div>
+          </div>
+        </div>
       </div>
+      {/* <FootCode /> */}
     </div>
   );
 }
